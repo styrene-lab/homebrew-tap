bd8bd5972c464dcdafff49b3270efeec65bd036c
Chris <72043878+cwilson613@users.noreply.github.com>
2026-06-08 14:14:30 +0000

    Update Flynt formula to 0.12.0

--- Formula/flynt.rb
+++ Formula/flynt.rb
@@ -13,12 +13,12 @@ class Flynt < Formula
 
     on_arm do
       url "https://github.com/styrene-lab/flynt/releases/download/v#{version}/flynt-v#{version}-linux-arm64.tar.gz"
-      sha256 "f89c9f3bff4e396fbff87155e3774602b914fbde6cbcf2c1d9833525b2bb2e3c"
+      sha256 "f5339848bb9d74b80bad426e88d56961ef6c438f716398d0ba9e413b81710c2d"
     end
 
     on_intel do
       url "https://github.com/styrene-lab/flynt/releases/download/v#{version}/flynt-v#{version}-linux-amd64.tar.gz"
-      sha256 "4e0ec5eebeb04bd2736b6b57bd6b13cbe3f26cb1579cf425eed5d34806c32f4f"
+      sha256 "194cc2e18180bfbefcc878beb822bdb78e112de5bf64b815081723747c6b34b9"
     end
   end
 
