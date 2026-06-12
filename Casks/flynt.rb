5ddc4c1c3e02e8dd51472d78c581f4adb9cf03a4
Chris <72043878+cwilson613@users.noreply.github.com>
2026-06-12 17:23:06 +0000

    Update Flynt cask to 0.12.9

--- Casks/flynt.rb
+++ Casks/flynt.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "flynt" do
-  version "0.12.8"
-  sha256 "34c13abac9e2b183383884468b66e0bb2ff55fd5e6bcd5a57464e18023240c2d"
+  version "0.12.9"
+  sha256 "6cb4b0c41dd9b79b0aca56366913482272768ddfef11a103d1048ec8a99c8f7d"
 
   url "https://github.com/styrene-lab/flynt/releases/download/v#{version}/Flynt-#{version}-macos.dmg"
   name "Flynt"
